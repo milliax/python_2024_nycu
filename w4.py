aaf12db71713ceaafb0c7012fd46f5de7b760eb5
milliax <wei@sivir.pw>
2024-10-09 10:59:41 +0800

    maybe correct?

--- w4.py
+++ w4.py
@@ -1,10 +1,30 @@
 row = 3
-col = 4 #Just for example
-#TODO
+col = 4  # Just for example
+
 ################################
 
+total_number = row * col
+ans = []
+prime_numbers = []
+number_now = 2
 
+# for i in range(total_number):
+while len(prime_numbers) <= total_number:
+    if number_now == 0:
+        prime_numbers.append(2)
+    else:
+        for j in prime_numbers:
+            if number_now % j == 0:
+                break
+        else:
+            prime_numbers.append(number_now)
+    
+    number_now += 1
 
+for i in range(row):
+    ans.append(prime_numbers[i * col: (i + 1) * col])
+
+print(ans)
 
 ################################
 # [[2, 3, 5, 7],
